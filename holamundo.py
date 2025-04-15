7ce3539673248e092aaad792daffbe67444ed87c
Orlando <o.posada23@gmail.com>
2025-04-14 23:33:54 -0600

    primeros inicios

--- holamundo.py
+++ holamundo.py
@@ -1,1 +1,4 @@
-print("hola mundo")
+sum1 = 255
+sum2 = 255
+total = sum1 + sum2
+print(total)
---
+++ input.py
@@ -0,0 +1,2 @@
+print("Tu nombre es: " + input("Dime tu nombre: ") + " " + input("Dime tu apellido: "))
+print("Yo vivo en: " + input("¿En qué pais vives? "))
