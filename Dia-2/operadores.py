c4293c14d953698cd710130c1a3c67286173908d
Orlando <o.posada23@gmail.com>
2025-04-22 23:03:44 -0600

    operadores

---
+++ Dia-2/operadores.py
@@ -0,0 +1,14 @@
+x = 6 
+y = 2
+z = 7
+
+print(f"{x} más {y} es igual a {x + y}")
+print(f"{x} menos {y} es igual a {x - y}")
+print(f"{x} por {y} es igual a {x * y}")
+print(f"{x} dividido entre {y} es igual a {x / y}")
+
+print(f"{z} dividido al piso de {y} es igual a {z//y}")# División entera sirve para saber cuantas veces cabe un número dentro de otro sin decimales
+print(f"{z} módulo {y} es igual a {z%y}")# Módulo sirve para saber el residuo de una división o saber si un número es divisible entre otro, para saber si es par o impar
+print(f"{x} elevado a la {y} es igual a {x**y}")# Potencia sirve para elevar un número a otro número
+print(f"{x} elevado a la {3} es igual a {x**3}")# Potencia sirve para elevar un número a otro número
+print(f"La raiz cuadrada de {x} es igual a {x**(1/2)}")# Raiz cuadrada sirve para sacar la raiz cuadrada de un número
