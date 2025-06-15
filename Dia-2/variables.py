ddd91c1a3aaf35fdcad4b401be4a90047b8b44e1
Orlando <o.posada23@gmail.com>
2025-06-15 13:26:17 -0600

    Avance dia 2

--- Dia-2/variables.py
+++ Dia-2/variables.py
@@ -1,2 +1,16 @@
+"""
 edad = 30
-print(edad)
+edad2 = 25
+print(edad + edad2)
+
+"""
+
+nombre = input('dime tu nombre: ')
+edad = input('dime tu edad: ')
+ocupacion = input('a que te dedicas: ')
+auto = input('que auto tienes: ')
+
+print("\nTu nombre es: " + nombre)
+print("Tu edad es: " + edad)
+print("Tu ocupacion es: " + ocupacion)
+print("Tu auto es: " + auto)
