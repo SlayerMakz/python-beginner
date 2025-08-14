a0df1f1d790690f091bd5e65c55536ba2b4a26ff
SlayerMakz <o.posada23@outlook.com>
2025-08-14 17:30:56 -0600

    List2

--- cursomidudev/flow_control/02-list.py
+++ cursomidudev/flow_control/02-list.py
@@ -34,3 +34,31 @@ print(lista2[-2]) # peras
 # Acceso a elementos con más indices.
 print(lista_de_listas[1][0])
 print(lista_de_listas[0][1])
+
+# Slicing (rebanado) de listas.
+lista4 = [1, 2, 3, 4, 5]
+print(lista4[1:4]) # [2, 3, 4] El ultimo indice sin incluir. 
+print(lista4[:3]) # [1, 2, 3] Esto devuelve los 3 primeros numeros.
+print(lista4[3:]) # Esto devuelve los ultimos numeros hasta el indice 3 [4, 5]
+print(lista4[:]) # Esto te devuelve una copia de la lista para guardarla o modificarla y tener un respaldo
+
+# Extras.
+lista5 = [1, 2, 3, 4, 5, 6, 7, 8]
+print(lista5[::2]) # Esto hace que se salte los indices pares e imprima los impares. 
+print(lista5[::-1]) # De vuelva la lista invertida.
+
+
+# Modificar una lista.
+lista5[0] = 20
+print(lista5)
+
+# Añadir elementos a una lista.
+lista1 = [1, 2, 3]
+
+# Forma larga y menos eficiente.
+lista1 = lista1 + [4, 5, 6]
+print(lista1)
+
+# Forma corta y más eficiente.
+lista1 += [7, 8, 9]
+print(lista1)
