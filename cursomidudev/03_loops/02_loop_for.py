ecf2497482af3340d35815b5284cea1306882be9
SlayerMakz <o.posada23@outlook.com>
2025-09-25 17:42:13 -0600

    avance bucles for

--- cursomidudev/03_loops/02_loop_for.py
+++ cursomidudev/03_loops/02_loop_for.py
@@ -29,3 +29,31 @@ numeros = [1, 2, 3]
 for letra in letras:
     for numero in numeros:
         print(f"{letra}{numero}")
+        
+###
+# Uso de break
+
+animales = ["perro", "gato", "raton", "loro", "pez", "canario"]
+for idx, animal in enumerate(animales): 
+    print(animal)
+    if animal == "loro": # perro, gato, raton, loro. Aquí termina el código.
+        print(f"El loro está escondido en el indice {idx}")
+        break
+
+# Continue 
+animales = ["perro", "gato", "raton", "loro", "pez", "canario"]
+for idx, animal in enumerate(animales): 
+    if animal == "loro":
+        continue
+    print(animal)
+    
+# Comprensión de listas. (list comprehension)
+animales = ["perro", "gato", "raton", "loro", "pez", "canario"]
+animales_mayus = [animal.upper() for animal in animales]
+print(animales_mayus)
+
+# muestra los numeros pares de una lista
+pares = [num for num in [1, 2 ,3, 4, 5, 6] if num % 2 == 0 ]
+print(pares)
+
+
---
+++ cursomidudev/03_loops/02_loop_for_excercises.py
@@ -0,0 +1,41 @@
+import os
+os.system("clear")
+# Ejercicio 1: Imprimir numeros pares. 
+# Imprime todos los numeros pares del 2 al 20 (inclusive) usando un bucle for
+print("----------------------------------")
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+for num in numeros: 
+    if num % 2 == 0:
+        print(num)
+print("----------------------------------")
+# Ejercicio 2: Calcular la media de una lista.
+# Dada la siguiente lista.
+numeros = [10, 20, 30, 40, 50]
+for num in numeros:
+    suma_total = sum(numeros)
+    media = suma_total / len(numeros)
+print(media)
+print("----------------------------------")
+# Ejercicio 3: Buscar el máximo de una lista. 
+# Dada la siguiente lista de numeros. 
+numeros = [15, 5, 25, 10, 20, 35, 87]
+for idx, num in enumerate(numeros): 
+    if num == max(numeros): #Sirve para conocer el numero mas grande
+        print(f"El numero máximo de esta lista es: {num}")
+print("----------------------------------")
+# Ejercicio 4: Filtrar cadenas por longitud.
+# Crea una lista que contenga solo las palabras con más de 5 letras.
+# Usando un bucle for y list comprehension
+palabras = [pal for pal in ["casa", "arbol", "sol", "elefante", "luna"] if len(pal) > 5]
+print(palabras)
+print("----------------------------------")
+# Ejercicio 5: Contar palabras que empiezan con una letra.
+# Pide al usuario que introduzca una letra. 
+# Cuenta cuantas palabras en la lista empiezan con esa letra.
+lista_palabras = ["casa", "arbol", "sol", "elefante", "luna", "coche"]
+letra = input("Ingresa una letra: ").lower() #Se pone el .lower para que sea indistintivo si el usuario pone una letra mayuscula o minuscula
+contador = 0 # Esta variable va a almacenar y guardar las palabras que lleven la letra.
+for palabra in lista_palabras:
+    if palabra.lower().startswith(letra): #Aqui usamos 2 funciones que es .lower() que va a leer la palabra sin importar si empieza con mayuscula o minuscula y el startswith nos ayudara a que también pueda verificar solamente las palabras que lleven la letra que ingreso el usuario.
+        contador += 1 #Esto nos permite agregar al contador e ir sumando las palabras que encuentre.
+print(f"Hay {contador} palabras que empiezan con la letra: {letra}")
